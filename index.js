1c4a8bd6e25589bab70ab696578598a56d90f13d
Vitor Marcelino <vitormarcelino93@gmail.com>
2019-04-10 09:06:15 -0300

    Add cache control

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
+
 const axios = require('axios');
 const moment = require('moment');
 const cron = require('node-cron');
 const config = require('./config');
+const fs = require('fs');
 // const Gpio = require('onoff').Gpio;
 
 // const relay = new Gpio(17, 'out');
@@ -10,8 +13,9 @@ var program = require('commander');
 
 program.option('--dev', 'Development Mode (Fake API)').parse(process.argv);
 
-var sunset = 0;
-var turnOffTime = moment(config.turnOffTime, 'h:mm:ss a').format('X');
+var sunset = config.cache.sunset ? moment(config.cache.sunset, 'h:mm:ss a').format('X') : 0;
+console.log(sunset);
+var turnOffTime = moment.utc(config.turnOffTime, 'h:mm:ss a').local().format('X');
 var state = false;
 
 requestSunsetTime();
@@ -31,7 +35,6 @@ function requestSunsetTime() {
 
   const api = program.dev ? config.api.dev : config.api.prod;
   const params = `?lat=${config.latitude}&lng=${config.longitude}`;
-  console.log(`${api}${params}`);
 
   axios.get(`${api}${params}`)
   .then(function (response) {
@@ -39,12 +42,12 @@ function requestSunsetTime() {
       var m = moment.utc(date, 'h:mm:ss a');
 
       sunset = m.local().format('X');
+      writeCache('sunset', m.local().format('h:mm:ss a'));
       handleLamp();
+      console.log(sunset);
 
   }).catch(function (error) {
     console.log(error);
-  }).then(function () {
-
   });
 }
 
@@ -63,6 +66,19 @@ function handleLamp() {
     console.log('Turn Off');
   }
 
+}
+
+function writeCache(key, value) {
+  let cache = config.cache ? config.cache : {} ;
+  cache[key] = value;
+  config.cache = cache;
+  fs.writeFile('config.json', JSON.stringify(config, null, 4), (err) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("Update cache");
+    }
+  });
 }
 
 
