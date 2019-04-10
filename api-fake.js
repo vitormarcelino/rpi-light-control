653d48cb934e6904b00c5da1c1daaf54a8de996b
Vitor Marcelino <vitormarcelino93@gmail.com>
2019-04-10 08:09:36 -0300

    Init

---
+++ api-fake.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = express();
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+app.get('/fake', function (req, res) {
+    let data = {
+        "results": {
+          "sunrise": "8:39:12 AM",
+          "sunset": "22:30:46 PM",
+          "solar_noon": "2:35:29 PM",
+          "day_length": "11:52:34",
+          "civil_twilight_begin": "8:17:53 AM",
+          "civil_twilight_end": "8:53:05 PM",
+          "nautical_twilight_begin": "7:53:07 AM",
+          "nautical_twilight_end": "9:17:51 PM",
+          "astronomical_twilight_begin": "7:28:24 AM",
+          "astronomical_twilight_end": "9:42:34 PM"
+        },
+        "status": "OK"
+      }
+    res.json(data);
+});
+
+var server = app.listen(3001, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
---
+++ index.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const moment = require('moment');
+const cron = require('node-cron');
+const config = require('./config');
+// const Gpio = require('onoff').Gpio;
+
+// const relay = new Gpio(17, 'out');
+
+
+var sunset = 0;
+var turnOffTime = moment(config.turnOffTime, 'h:mm:ss a').format('X');
+var state = false;
+
+requestSunsetTime();
+
+cron.schedule("* * * * *", () => {
+  requestSunsetTime();
+});
+
+cron.schedule("* * * * *", () => {
+  handleLamp();
+});
+
+
+function requestSunsetTime() {
+
+  // const api = "https://api.sunrise-sunset.org/json";
+  const api = "http://localhost:3001/fake";
+  const params = `?lat=${config.latitude}&lng=${config.longitude}`;
+
+  axios.get(`${api}${params}`)
+  .then(function (response) {
+      var date = response.data.results.sunset;
+      var m = moment.utc(date, 'h:mm:ss a');
+
+      sunset = m.local().format('X');
+      handleLamp();
+
+  }).catch(function (error) {
+    console.log(error);
+  }).then(function () {
+
+  });
+}
+
+function handleLamp() {
+  var now = moment().format("X");
+
+  if (now >= sunset && now < turnOffTime && !state) {
+    state = true;
+    // relay.writeSync(state);
+    console.log('Ligando a lampada ');
+  }
+
+  if (now >= turnOffTime && state) {
+    state = false;
+    // relay.writeSync(state);
+    console.log('Desligando a lampada');
+  }
+
+}
+
+
